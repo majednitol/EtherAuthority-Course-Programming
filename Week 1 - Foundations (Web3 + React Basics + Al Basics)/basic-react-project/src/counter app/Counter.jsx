a529df57cbae8ddc7ad6d25474432fede2c92449
majednitol <majedrahman7@gmail.com>
2025-12-16 19:41:18 +0600

    added counter app

---
+++ Week 1 - Foundations (Web3 + React Basics + Al Basics)/basic-react-project/src/counter app/Counter.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+
+    return (
+        <div className="counter-container">
+            <h1>Counter App</h1>
+            <h2>{count}</h2>
+            <div>
+                <button onClick={increment}>Increment</button>
+                <button onClick={decrement}>Decrement</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
